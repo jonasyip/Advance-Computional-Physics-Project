2a831ca522a9abd2aa7f5eff8bf0fc40e660c2ae
Jonas Yip <40857304+jonasyip@users.noreply.github.com>
2022-10-27 16:50:55 +0100

    added cdef

--- mini_project/Direct approach/Cython/nbody_system_pyx.pyx
+++ mini_project/Direct approach/Cython/nbody_system_pyx.pyx
@@ -4,49 +4,66 @@ os.environ["NUMEXPR_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
 
 cimport numpy as np
-cimport matplotlib.pyplot as plt
-cimport matplotlib.animation as animation
+#cimport matplotlib.pyplot as plt
+#cimport matplotlib.animation as animation
+import pandas as pd
 
 
 cdef class body:
-    cdef __init__(self, name, mass, x, y, z, vx, vy, vz):
-        cdef:
-            char self.name = name            #Name of the body
-            double self.mass = mass            #Mass of the body   (kg)
 
-            double self.x = x                  #x position         (km)
-            double self.y = y                  #y position         (km)
-            double self.z = z                  #z position         (km)
+    cdef:
+        char name            #Name of the body
+        double mass            #Mass of the body   (kg)
 
-            double self.vx = vx                #x velocity         (km/s)
-            double self.vy = vy                #y velocity         (km/s)
-            double self.vz = vz                #z velocity         (km/s)
+        double x                  #x position         (km)
+        double y                  #y position         (km)
+        double z                 #z position         (km)
+
+        double vx              #x velocity         (km/s)
+        double vy              #y velocity         (km/s)
+        double vz               #z velocity         (km/s)
+
+    def __init__(self, name, mass, x, y, z, vx, vy, vz):
+        self.name = name            #Name of the body
+        self.mass = mass            #Mass of the body   (kg)
+
+        self.x = x                  #x position         (km)
+        self.y = y                  #y position         (km)
+        self.z = z                  #z position         (km)
+
+        self.vx = vx                #x velocity         (km/s)
+        self.vy = vy                #y velocity         (km/s)
+        self.vz = vz                #z velocity         (km/s)
 
     cdef update(self, timestep, acceleration):
         cdef:
-            double self.ax = acceleration[0]
-            double self.ay = acceleration[1] 
-            double self.az = acceleration[2] 
+            double ax, ay, az
+            double new_vx, new_vy, new_vz
+            double new_x, new_y, new_z
+
+        self.ax = acceleration[0]
+        self.ay = acceleration[1] 
+        self.az = acceleration[2] 
 
         #v_i_new = v_i + (a_i * t)
-            double new_vx = self.vx + (self.ax * timestep)
-            double new_vy = self.vy + (self.ay * timestep)
-            double new_vz = self.vz + (self.az * timestep)
+        new_vx = self.vx + (self.ax * timestep)
+        new_vy = self.vy + (self.ay * timestep)
+        new_vz = self.vz + (self.az * timestep)
 
         #r_i = r_i + (v_i * t)
-            double new_x = self.x + (new_vx * timestep)
-            double new_y = self.y + (new_vy * timestep)
-            double new_z = self.z + (new_vz * timestep)
+        new_x = self.x + (new_vx * timestep)
+        new_y = self.y + (new_vy * timestep)
+        new_z = self.z + (new_vz * timestep)
 
         #Write new positions
-            double self.x = new_x
-            double self.y = new_y
-            double self.z = new_z
+        self.x = new_x
+        self.y = new_y
+        self.z = new_z
 
         #Write new velocities
-            double self.vx = new_vx
-            double self.vy = new_vy
-            double self.vz = new_vz
+        self.vx = new_vx
+        self.vy = new_vy
+        self.vz = new_vz
 
     def position(self):
         return np.array([self.x, self.y, self.z])
@@ -75,7 +92,7 @@ class system:
 
     def update(self, timestep):
         cdef:
-        int count
+            int count
             double G_const
             double[:,:] acceleration, a_array 
             double[:] a_i, r1, r2, r_diff
@@ -99,14 +116,15 @@ class system:
 
     def run(self, timestep, steps, display=False):
 
-        for step in range(0, steps):
-            self.update(timestep)
-            if (display == True):
-                fig = plt.figure()
-                ax = plt.axes(projection = '3d')
-                self.display(ax)
-                save = "%s.png" % step
-                plt.savefig(save)
+        self.update(timestep)
+        # for step in range(0, steps):
+        #     self.update(timestep)
+        #     if (display == True):
+        #         fig = plt.figure()
+        #         ax = plt.axes(projection = '3d')
+        #         self.display(ax)
+        #         save = "%s.png" % step
+        #         plt.savefig(save)
                 
 
 
@@ -119,7 +137,7 @@ class system:
 
 
 
-def main(timestep, steps)
+def main(timestep, steps):
     file_name = r"mini_project\Direct approach\Cython\system.csv"
     df = pd.read_csv(file_name, header=1)
     solar_system = np.array(df)
--- mini_project/Direct approach/Cython/setup_nbody_system.py
+++ mini_project/Direct approach/Cython/setup_nbody_system.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup("nbody_system_pyx", ext_modules=cythonize("setup_nbody_system_pyx.pyx"))
+setup("nbody_system_pyx", ext_modules=cythonize("nbody_system_pyx.pyx"))
