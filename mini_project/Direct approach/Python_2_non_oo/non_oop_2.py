7b50a7007496ba8e3935d874de6f5054dc75d258
Jonas Yip <40857304+jonasyip@users.noreply.github.com>
2022-11-03 12:51:22 +0000

    thursday push

--- mini_project/Direct approach/Python_2_non_oo/non_oop_2.py
+++ mini_project/Direct approach/Python_2_non_oo/non_oop_2.py
@@ -5,12 +5,12 @@ from matplotlib import animation
 
 def run(timestep, iterations):
     #Constants
-    G_CONST = 6.67430E-11
+    G_CONST = 6.67430E-20
 
     #Load initial system
     path = r"C:\Users\Student\OneDrive\Bristol University\Physics Year 4\Advanced Computational Physics\Advance-Computional-Physics-local-machine-1\mini_project\Direct approach\Python_2_non_oo\system.csv"
     initial_system = np.loadtxt(path, skiprows=2, delimiter=',', dtype=np.float64)
-    print(initial_system)
+    # print(initial_system)
     global total_bodies
     total_bodies = len(initial_system)
 
@@ -20,18 +20,15 @@ def run(timestep, iterations):
     nbody_system_w_time = np.zeros(iterations+1, dtype=object)
     nbody_system_w_time[0] = initial_system
 
-    
-
-    for j in range(iterations):
+    for count_k in range(1, iterations):
         count_j = 0
-        count_k = 1
         a_to_update = np.zeros(total_bodies, dtype=object)
         for body1 in nbody_system:
             count = 0
             a_array = np.zeros(((total_bodies-1), 3))  
             for body2 in nbody_system:
                 if (body1[0] != body2[0]):
-                    print("\nbody1 %s , body2 %s" % (body1, body2))
+                    # print("\nbody1 %s , body2 %s" % (body1, body2))
                     r1 = np.array([body1[2], body1[3], body1[4]])
                     r2 = np.array([body2[2], body2[3], body2[4]])
                     r_diff = np.subtract(r1, r2)
@@ -44,53 +41,88 @@ def run(timestep, iterations):
             a_to_update[count_j] = acceleration
             count_j += 1
 
-        print(a_to_update)
-        print("nbody_system \n", nbody_system)
+        print("a_to_update ", a_to_update)
+        # print("nbody_system \n", nbody_system)
         for body1, i, accel in zip(nbody_system, range(total_bodies), a_to_update):
-            new_x = body1[2] + (body1[5] + accel[0]*timestep) * timestep    #new_x = odd_x + (velocity + accel*time)*time
-            new_y = body1[3] + (body1[6] + accel[1]*timestep) * timestep
-            new_z = body1[4] + (body1[7] + accel[2]*timestep) * timestep
- 
+            new_vx = body1[5] + accel[0]*timestep
+            new_vy = body1[6] + accel[1]*timestep
+            new_vz = body1[7] + accel[2]*timestep
+            
+            new_x = body1[2] + (new_vx * timestep)    #new_x = odd_x + (velocity + accel*time)*time
+            new_y = body1[3] + (new_vy * timestep)
+            new_z = body1[4] + (new_vz * timestep)
+
             nbody_system[i, 2] = new_x
             nbody_system[i, 3] = new_y
             nbody_system[i, 4] = new_z
+
+            nbody_system[i, 5] = new_vx
+            nbody_system[i, 6] = new_vy
+            nbody_system[i, 7] = new_vz
+
         nbody_system_w_time[count_k] = nbody_system
-        count_k += 1
-        print("i ", j)
-        print("\n nbody_system_w_time ", nbody_system_w_time)
-        print("\n \n \n")
+        # print("count_k ", count_k)
+        # print("\n nbody_system_w_time ", nbody_system_w_time)
+        # print("\n \n \n")
         
 
-timestep = 86400 
+timestep = 86400
 iterations = 10
 run(timestep, iterations)
 
-def animate(i):
-    ax.clear()
-    system_t = nbody_system_w_time[i]
-    print(system_t, )
-    # graph.set_array = (system_t[:, 2] , system_t[:, 3], system_t[:, 4])
-    title.set_text('Time={} s'.format(i*timestep))
-    ax.scatter(system_t[:, 2] , system_t[:, 3], system_t[:, 4])
+# # def animate(i):
+# #     ax.clear()
+# #     system_t = nbody_system_w_time[i]
+# #     # print(system_t)
+# #     # graph.set_array = (system_t[:, 2] , system_t[:, 3], system_t[:, 4])
+# #     ax.title('Time={} s'.format(i*timestep))
+# #     ax.set_data(system_t[:, 2] , system_t[:, 3], system_t[:, 4])
 
-savepath = r"C:\Users\Student\OneDrive\Bristol University\Physics Year 4\Advanced Computational Physics\Advance-Computional-Physics-local-machine-1\mini_project\Direct approach\Python_2_non_oo\run"
-print(nbody_system_w_time[3])
+# def animate(i):
+#     for k in range(total_bodies):
+#         stars[k].set_data(stars[k][:,0][i], stars[k][:,1][i])
+#         stars[k].set_3d_properties(stars[k][:,2][i])
 
-# for i in range(len(nbody_system_w_time)):
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111, projection='3d')
-#     title = ax.set_title(' title ')
-#     # ax.set_xlim((-5E9, 5E9))
-#     # ax.set_ylim((-5E9, 5E9))
-#     # ax.set_zlim((-5E9, 5E9))
+#         trajectories[k].set_data(stars[k][:,0][:i+1],stars[k][:,1][:i+1])   
+#         trajectories[k].set_3d_properties(stars[k][:,2][:i+1])
+#     return np.concatenate([stars, trajectories])
+
+# fig = plt.figure()
+# ax = fig.add_subplot(projection = '3d')
+# # ax.set_xlim((-5E25, 5E25))
+# # ax.set_ylim((-5E25, 5E25))
+# # ax.set_zlim((-5E25, 5E25))
+
+# stars = np.array([plt.plot([],[],[]) for k in range(total_bodies)])
+# trajectories = np.array([plt.plot([],[], [])[0] for k in range(total_bodies)])
 
 
-#     system_t = nbody_system_w_time[i]
-#     ax.scatter(system_t[:, 2] , system_t[:, 3], system_t[:, 4])
-#     save = "%s\%s.png" % (savepath, i)
-#     plt.savefig(save)
-# # frames_t = iterations-2
-# # ani = animation.FuncAnimation(fig, animate, 
-# #                                interval=200, frames=frames_t, blit=False)
+
+# frames_t = iterations-1
+# system_t = nbody_system_w_time[0]
+# ax.scatter(system_t[:, 2] , system_t[:, 3], system_t[:, 4])
+# ani = animation.FuncAnimation(fig, animate, interval=200, frames=frames_t, blit=False)
 
 # plt.show()
+print("\n nbody_system_w_time", nbody_system_w_time)
+system_t = nbody_system_w_time[2]
+print(system_t)
+print(system_t[0])
+print("\n")
+print(system_t[0, 1])
+
+savepath = r"C:\Users\Student\OneDrive\Bristol University\Physics Year 4\Advanced Computational Physics\Advance-Computional-Physics-local-machine-1\mini_project\Direct approach\Python_2_non_oo\run"
+for system_t, i in zip(nbody_system_w_time, range(len(nbody_system_w_time))):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    title = ax.set_title('{}'.format(i*timestep))
+    # ax.set_xlim((-5E9, 5E9))
+    # ax.set_ylim((-5E9, 5E9))
+    # ax.set_zlim((-5E9, 5E9))
+
+
+    system_t = nbody_system_w_time[i]
+    ax.scatter(system_t[:, 2] , system_t[:, 3], system_t[:, 4])
+    save = "%s\%s.png" % (savepath, i)
+    plt.savefig(save)
+    plt.close()
